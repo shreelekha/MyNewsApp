803ad3e95ac3c4f4e865104b57ff3ba9ab716c5b
Shreelekha Tanna <shree.tanna@gmail.com>
2016-11-01 12:15:04 -0700

    set up prototype cell in table view home page

---
+++ MyNewsApp/NewsItemTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  NewsItemTableViewCell.swift
+//  MyNewsApp
+//
+//  Created by Rakshit Majithiya on 2016-10-28.
+//  Copyright © 2016 Shreelekha Tanna. All rights reserved.
+//
+
+import UIKit
+
+class NewsItemTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var newsImage: UIImageView!
+    @IBOutlet weak var newsLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+}
